cba2a0e75fe4f901cf54fb87292c92637788ca56
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-09-18 21:07:38 +0700

    Revert "init"
    This reverts commit b3a5c2bb00c8f9ea0d2e2e7e187a008f6768e75c.

--- kucut/__init__.py
+++
@@ -1,2 +0,0 @@
-from simple_kucut_wrapper import SimpleKucutWrapper
-from kucut import *
--- kucut/nospace.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-for line in sys.__stdin__.readlines():
-    print ''.join(line.strip().split()).replace('_','')
